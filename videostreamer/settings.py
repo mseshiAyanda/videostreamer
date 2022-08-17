5a8212caeece1fcf879ab4a264e4d6dbd0f159ad
Ayanda <68080779+mseshiAyanda@users.noreply.github.com>
2022-08-17 19:39:18 +0200

    Update settings.py

--- videostreamer/settings.py
+++ videostreamer/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_URL = 'static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 
